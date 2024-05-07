de8fc60167368f411a1ce85a66418120676dfc14
matheuspalhanolunga2 <164065060+matheuspalhanolunga2@users.noreply.github.com>
2024-05-07 14:04:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const cadastroForm = document.getElementById('cadastroForm');
-  const mensagemCadastro = document.getElementById('mensagemCadastro');
-
-  cadastroForm.addEventListener('submit', function (event) {
-    event.preventDefault(); // Evita o envio do formulário
-    mensagemCadastro.textContent = 'Sua inscrição foi realizada!';
-    mensagemCadastro.style.display = 'block';
-  });
-});
 
 
 
